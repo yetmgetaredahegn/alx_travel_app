e22be0a094bf632c8357d3d219c068b8a032bcfd
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-09-20 23:09:44 +0300

    milestone 1

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def home(request):
+    return JsonResponse({"message": "Welcome to ALX Travel API"})
